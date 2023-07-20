59443270948c25d8d449bbf6d8b7023dcfcf5659
Jrativa <jrativa@unal.edu.co>
2023-07-20 00:32:03 -0500

    banner finished

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import headerImg from "../assets/images/header-img.svg";
+import headerImg from "../assets/images/cat_programming_bg_removed.png";
 import { useEffect, useState } from "react";
 
 export const Banner = () => {
@@ -43,12 +43,13 @@ export const Banner = () => {
         <section className="banner" id="home">
             <Container>
                 <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7}>
+                    <Col xs={12} md={6} className="banner-left">
                         <span className="tagline">Welcome to my portfolio</span>
                         <h1>{`Hi, I am Jose Luis`} <span className="wrap">{text}</span></h1>
-                        <button onClick={()=>console.log('connect')}>Lets build amazing things :)<ArrowRightCircle size={25} /></button>
+                        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum</p>
+                        <button onClick={()=>console.log('connect')}>{`Let's make amazing things :)`}<ArrowRightCircle size={25} /></button>
                     </Col>
-                    <Col xs={12} md={6} xl={7} className="banner-right">
+                    <Col xs={12} md={6} className="banner-right">
                         <img src={headerImg} alt="Header image" />
                     </Col>
                 </Row>
