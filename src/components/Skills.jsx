f4fe956b1ef6a8cd234ea7764a55939f0144eb9a
Jrativa <jrativa@unal.edu.co>
2023-07-21 01:03:03 -0500

    skills section

---
+++ src/components/Skills.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import colorSharp from '../assets/images/color-sharp.png';
+import meter1 from "../assets/images/meter1.svg";
+import meter2 from "../assets/images/meter2.svg";
+import meter3 from "../assets/images/meter3.svg";
+import {SKILLS} from '../shared/skills'
+
+
+
+export const Skills = () => {
+    
+    const Skills = SKILLS.map((skill) => {
+        return (
+            <div key={skill.id} className='skill-item'>
+                <img src={skill.image} alt="Image" />
+                <h5>{skill.name}</h5>
+            </div>
+        );
+    });
+    console.log(Skills);
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+    return (
+        <section className='skill' id='skills'>
+            <Container>
+                <Row>
+                    <Col xs={12}>
+                        <div className='skill-bx'>
+                            <h2>
+                                Skills
+                            </h2>
+                            <p>Holaaaa</p>
+                            <Carousel responsive={responsive} infinite={true} className='skill-slider'>
+                                {Skills}
+                            </Carousel>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-left" src={colorSharp} alt="Image" />
+        </section>
+    );
+};
